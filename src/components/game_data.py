50819131e370efc58adcece4d86420de3da87bb4
Adam <adam.shannon02@gmail.com>
2023-10-28 10:22:11 +0100

    Second Item and minor logic change

--- src/components/game_data.py
+++ src/components/game_data.py
@@ -2,10 +2,16 @@ class item():
     def __init__(self, durability):
         self.name = self.__class__.__name__
         self.durability = durability
+    def use(self):
+        self.durability = self.durability - 1
 
 class Shovel(item):
     def __init__(self):
-        super().__init__(1)
+        super().__init__(5)
+
+class Lamp(item):
+    def __init__(self):
+        super().__init__(15)
 
 
-inventory = [Shovel()]
+inventory = [Shovel(), Lamp()]
