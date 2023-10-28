e24cb7155bfb6a6c72581c923375aa6af9027bd0
Adam <adam.shannon02@gmail.com>
2023-10-28 16:08:19 +0100

    Almost done generation

--- src/components/generator.py
+++ src/components/generator.py
@@ -1,5 +1,5 @@
 import g4f
-from g4f.Provider import ChatBase as prov
+from g4f.Provider import ChatgptAi
 import regex as re
 
 #generation function
@@ -10,17 +10,15 @@ def generate(prompt):
     try:
        response = g4f.ChatCompletion.create(
        model="gpt-3.5-turbo",
+       provider=g4f.Provider.ChatgptAi,
        messages=[{"role": "user", "content": prompt}],
-       stream=False,
+       stream=g4f.Provider.ChatgptAi.supports_stream,
        )
-       riddle = re.search("\{(\w+)\}",response).groups()[0]
-       answer = re.search("\((\w+)\)",response).groups()[0]
+
+       fulltext = "".join([i for i in response])
+
+       riddle = re.search("\{(\w+)\}", fulltext)
+       answer = re.search("\((\w+)\)", fulltext)
        return (riddle,answer) 
     except:
         generate(prompt)
-
-    #if any((match := extr.match(x)) for x in response):
-     #  if (match.group(0) == "None"):
-      #     generate("context")
-       #else:
-        #return match.group(0)
