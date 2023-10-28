dd575188586f4a3179143cf0b4bad152004e51fa
yCount <yiit.sayar@yandex.com>
2023-10-28 12:10:33 +0200

    coundown timer added

--- src/game.py
+++ src/game.py
@@ -1,16 +1,25 @@
 import pygame
 from components.state import First
 import components.game_data as gd
+import time
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 state =  First(screen, gd)
+COUNTDOWN_TIME = 120
+
+t_start = time.perf_counter()
 
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
+    t_stop = time.perf_counter()
+    
+    if COUNTDOWN_TIME <= (t_stop - t_start):
+        running = False
+        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
